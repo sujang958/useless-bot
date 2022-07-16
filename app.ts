6706c7a9e4e7b798d970ebe1b04eb3178accf9c4
sujang958 <enter7377@naver.com>
2022-07-16 20:45:40 +0900

    feat: add useless bot

---
+++ app.ts
@@ -0,0 +1,24 @@
+import "dotenv/config"
+
+import { Client, Intents } from "discord.js"
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+})
+
+client.on("ready", (clt) => {
+  console.log(clt.user.username)
+})
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return
+  if (!message.guild) return
+
+  if (message.channelId == "997829841275600986")
+    message.reply({
+      content: "ㅗ",
+      allowedMentions: { parse: [] },
+    })
+})
+
+client.login(process.env.BOT_TOKEN)
