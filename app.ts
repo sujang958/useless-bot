e3694f0346ffe3521c512bcb0c4f45738a122a45
sujang958 <enter7377@naver.com>
2022-07-18 21:50:55 +0900

    feat: add setting channel

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
 import "dotenv/config"
 
 import { Client, Intents } from "discord.js"
+import { MongoClient } from "mongodb"
+
+const DBClient = new MongoClient(
+  `mongodb://sujang:${process.env.DB_PW}@cluster0-shard-00-00.qcdis.mongodb.net:27017,cluster0-shard-00-01.qcdis.mongodb.net:27017,cluster0-shard-00-02.qcdis.mongodb.net:27017/?ssl=true&replicaSet=atlas-9jt0g9-shard-0&authSource=admin&retryWrites=true&w=majority`
+)
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -10,11 +15,47 @@ client.on("ready", (clt) => {
   console.log("Logined:", clt.user.username)
 })
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
   if (message.author.bot) return
   if (!message.guild) return
 
-  if (message.channelId !== "997829841275600986") return
+  const collection = DBClient.db("usl").collection("usls")
+
+  if (message.content.startsWith("!ㅗ설정 ")) {
+    const setId = message.content
+      .split("!ㅗ설정 ")[1]
+      .replace("<#", "")
+      .replace(">", "")
+
+    await collection.updateOne(
+      {
+        guildId: message.guildId,
+      },
+      {
+        $set: {
+          guildId: message.guildId,
+          channelId: setId,
+        },
+      },
+      { upsert: true }
+    )
+    message.react("🖕")
+    message.reply({
+      content: "완료",
+      allowedMentions: { parse: [] },
+    })
+    return
+  }
+
+  const list = await collection.find().toArray()
+
+  if (
+    !list.find(
+      (item) =>
+        item.guildId === message.guildId && item.channelId === message.channelId
+    )
+  )
+    return
 
   message.react("🖕")
   message.reply({
@@ -23,4 +64,11 @@ client.on("messageCreate", (message) => {
   })
 })
 
-client.login(process.env.BOT_TOKEN)
+DBClient.connect()
+  .then(() => client.login(process.env.BOT_TOKEN))
+  .then(() =>
+    console.log(
+      new Date().toISOString(),
+      "Datebase connected, starting the bot"
+    )
+  )
